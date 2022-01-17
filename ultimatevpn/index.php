c228ffc91f1e1b50ba463e16196b403e4436d35e
parthpansuriya <37246367+parthpansuriya@users.noreply.github.com>
2022-01-17 13:55:38 +0530

    Update index.php

--- ultimatevpn/index.php
+++ ultimatevpn/index.php
@@ -1,27 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- Meta Tags For Seo + Page Optimization -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="author" content="Themes Industry">
-        <!-- description -->
-        <meta name="description" content="App landing Page is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and HTML5 template with 14 ready home page demos.">
-        <!-- keywords -->
-        <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, faq">
         <!-- Page Title -->
-        <title>VPN Master - Free Unlimited VPN Proxy</title>
+        <title>Unlimite VPN Master</title>
         <!-- Favicon -->
-        <link rel="icon" href="./img/favicon.png">
-        <!--css links-->
-        <link rel="stylesheet" href="./assets/css/bundle.min.css">
-        <link rel="stylesheet" href="./assets/css/wow.css">
-        <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
-        <link rel="stylesheet" href="./assets/css/line-awesome.min.css">
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    </head>
+     </head>
 
     <body data-spy="scroll" data-target=".navbar" data-offset="90">
         <div><p>Hello</p></div>
